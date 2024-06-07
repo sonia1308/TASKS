ff37b8471d31ab10d1d5e632d8a189c18599a229
sonia1308 <klodatanasia@gmail.com>
2024-06-07 17:07:16 +0400

    conditional statements practiced

---
+++ tasks/JS-intro/app.js
@@ -0,0 +1,85 @@
+// console.log("t1");
+// let a = "My Name IS Sonia"
+// console.log(a);
+
+// console.log("t2");
+
+// console.log("t3");
+// let va = "2user"
+// let vb = "c@rd"
+// let vc = "person"
+// let vd = "title color"
+// let ve = "console"
+// let vf = "$add"
+// console.log( vc, vd, ve);
+
+// console.log("t4");
+
+// let bookPrice = 750;
+// let bookAmount = 14;
+
+// console.log( bookPrice * bookAmount);
+
+// console.log("t5");
+// let m=1 
+// let s=1
+// let km = 1000*m
+// let h = 3600*s
+// let kmhSpeed = 10
+// let msSpeed = kmhSpeed / 3600*1000
+// console.log( kmhSpeed ,"kmhSpeed =", msSpeed, 'msSpeed');
+// console.log("t6");
+// let  result = prompt("Enter your age", []); 
+// let result2 = parseInt(result)
+// console.log((2024 - result)*365)
+// console.log("t7")
+// let num = 14
+// console.log(num**5);
+// console.log(num%3);
+// console.log("t8");
+// let firstName = "Sonia";
+// let lastName = "Hamidova";
+// console.log("Sizin adınız ", firstName , lastName, ". Tanışlığımıza çox şadam!");
+// console.log("t9");
+// let fullName = "John Doe";
+// console.log();
+
+
+/* function hesabla(){
+    
+} */
+// let login= prompt("Enter your login info")
+// if (login === null) {
+//     console.log("Cancelled.");
+// }
+
+// else if (login=== "Admin"){
+// let password= prompt("Password?")
+//     if (password === null) {
+//         console.log("Cancelled.");
+//     }
+//     else if(password = "The Master"){
+//         console.log("Welcome!");
+//     }
+//     else{
+//         console.log("Wrong password");
+//     }
+// }
+//     else{
+//         console.log("I dont know u");
+//     }
+
+// let eResult = +prompt("Enter your exam results")
+// if (50>=eResult) {
+//     console.log("I have bad news foryou...");
+// }
+// else if (85>=eResult && eResult>50){
+//     console.log("Good");
+// }
+// else  if(100>=eResult && 85>eResult){
+//     console.log("Exellent");
+// }
+// else{
+//     console.log("Try again next year");
+// }
+

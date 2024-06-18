a77055fcd80dd63e56a2ec65b6af3d77770e4885
sonia1308 <klodatanasia@gmail.com>
2024-06-18 16:53:47 +0400

    object task1,2,3

---
+++ tasks/JS-objects/app.js
@@ -0,0 +1,24 @@
+//#region 
+/* let user = {
+    name: 'Sonia',
+   surname:'Hamidova',
+   gender: 'female',
+   birthday: '13.08.2005'
+       }
+   
+   console.log(birthday);
+delete user.birthday
+user.age = '18'
+console.log(user) */
+//#endregion
+
+
+//#region 
+/* let user={
+    name: 'Rasim',
+    surname: 'Abbasov'
+    }
+    let userEntires= object.entires(user)
+    console.log (userEntires) */
+//#endregion
+

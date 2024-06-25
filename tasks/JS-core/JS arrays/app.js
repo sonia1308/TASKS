250ff80090ee005cbdc23c9da37bde4511656a5d
sonia1308 <klodatanasia@gmail.com>
2024-06-25 14:43:37 +0400

    array tasks done

---
+++ tasks/JS-core/JS arrays/app.js
@@ -0,0 +1,42 @@
+//#region homeTask1-2
+// let groupmates = [ `person-a`,`person-b`,`person-c`]
+// // groupmates.push(`person-d`)
+// // groupmates.shift()
+// // console.log(groupmates);
+// groupmates[1]=`Classified`
+// console.log(groupmates);
+// //#endregion
+
+//#region homeTask3
+// const numbers = [12,432,13,765,22]
+// console.log(numbers);
+// let newNumbers=[]
+// for (let i = 0; i < numbers.length; i++) {
+//     newNumbers.push(+numbers[i]*2)
+// }
+// console.log(newNumbers);
+// console.log(numbers);
+//#endregion
+
+//#region homeTask4
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jdi']  
+// let episode4 = `Episode 4: ${arr[0]}`
+// console.log(episode4);
+// let episode5 = `Episode 5: ${arr[1]}`
+// console.log(episode5);
+// let episode6 = `Episode 6: ${arr[2]}`
+// console.log(episode6);
+// //#endregion
+
+//#region homeTask5
+// let userInput = prompt(`enter your numbers seperated with ,`)
+// const numbers = userInput.split(`,`)
+// let result =Math.min(...numbers)
+// console.log(result);
+//#endregion
+
+//#region homeTask6
+// let numbers = [32,31,34,36,31]
+// console.log(numbers.join(`;`));
+//#endregion
+
--- tasks/JS-core/JS-strings2/app.js
+++ tasks/JS-core/JS-strings2/app.js
@@ -70,3 +70,4 @@
 // }
 // uncommon("xyab","xzca")
 //#endregion
+

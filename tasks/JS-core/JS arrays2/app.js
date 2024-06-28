58bc9230e4bbac66ff8cfb5e14d9d3775f8aad1e
sonia1308 <klodatanasia@gmail.com>
2024-06-28 17:55:48 +0400

    array2 tasks done

---
+++ tasks/JS-core/JS arrays2/app.js
@@ -0,0 +1,153 @@
+//#region homeTask1
+// let numbers = [12, 32, 235, 15, 1, 6, 5, 213, 14];
+// console.log(Math.max(...numbers));
+//#endregion
+
+//#region homeTask2
+// let employees = [
+//   ["Jaylee Macy", "marketing"],
+//   ["John Smith", "management"],
+//   ["Blossom Hartley", "design"],
+//   ["Austin Carpenter", "marketing"],
+//   ["Joan Knowles", "development"],
+//   ["Sally Nunez", "management"],
+//   ["Laurel Ward", "development"],
+//   ["Lark Simon", "marketing"],
+//   ["Jane Stone", "management"],
+//   ["Courtney Olson", "development"],
+// ];
+// let developers = [];
+// function findDevelopers(employees) {
+//   employees.forEach((element) => {
+//     if (element.includes(`development`)) {
+//       developers.push(element);
+//     }
+//   });
+//   console.log(developers);
+//   return developers;
+// }
+// findDevelopers(employees);
+//#endregion
+
+//#region homeTask3
+// let array = [
+//   1, 6, 34, 5, 1, -4, 54, 76, 23, 65, 3, 50, 45, 100, 2346, -52, 6545, 0, 45,
+//   22,
+// ];
+// numArray.sort(function (a, b) {
+//   return a - b;
+// });
+// console.log(numArray);
+//#endregion
+
+//#region homeTask4
+// let numbers = [12, -32, 235, 15, 1, 6, 5, 213, 14];
+// let num = numbers.splice(`,`)
+// let result = []
+// console.log(num);
+// console.log(numbers);
+// function findNegative(num) {
+//     for (let i = 0; i < num.length; i++) {
+//         if (Math.min(...num)<0) {
+//             result+= unshift()
+//         }
+//     }console.log(result);
+//     return result
+// }
+
+// findNegative(numbers)
+// let numbers = [12, -32, 235, 15, 1, 6, -5, 213, 14];
+// let result = [];
+// function facilis(numbers) {
+//   numbers.forEach((element) => {
+//     if (element < 0) {
+//       result.unshift(element);
+//     } else {
+//       return;
+//     }
+//   });
+//   if (result.length>0) {
+//     console.log(result);
+//     return result;
+//   }else{
+//     console.log(`you have not eneterd any negative numbers`);
+//   }
+// }
+// facilis(numbers);
+//#endregion
+names.forEach(printWithDashes);
+names.forEach(printWithHearts);
+names.forEach(printWithIndex);
+function printWithDashes(text) {
+    console.log(`-`*13);
+}
+//#region homeTask5
+
+//#endregion
+
+//#region homeTask6
+// function kebabToUpper(userInput) {
+//     const input = String(userInput)
+//     const upper = input.toUpperCase().split('-').join(`_`)
+//     console.log(upper);
+// }
+// kebabToUpper(`erjkgwh-ewge-gew`)
+//#endregion
+
+// #region homeTask7
+// const input = [1, 5, -7, 3, -9, 4, -6, 2];
+// const output = input.map(changed);
+// function changed(value, index, array) {
+//   if (value < 0) {
+//     return Math.abs(value);
+//   } else {
+//     return value * 2;
+//   }
+// }
+// console.log(output);
+//#endregion
+
+//#region homeTask8
+// let namesArray = [
+//   "Alice",
+//   "Bob",
+//   "Catherine",
+//   "David",
+//   "Eva",
+//   "Andrew",
+//   "Frank",
+//   "Anna",
+//   "George",
+//   "Alex",
+// ];
+// const aNames = namesArray.filter(filterNames);
+// function filterNames(value, index, array) {
+//     if (value[0]===`A`) {
+//         return value
+//     }
+// }
+// console.log(aNames);
+//#endregion
+
+//#region homeTask9
+// const pricesArray = [
+//   12.99, 24.95, 9.99, 34.5, 19.99, 42.75, 8.49, 15.0, 28.75, 10.99,
+// ];
+// // const pricesAndTaxes = pricesArray.map((priceAndTax)=>{
+// //     function priceAndTax( index, array) {
+// //         pricesArray.forEach((value) => {
+// //             return `Price =${value} and tax is ${20/100*value}`
+// //         });
+// //     }  
+// // })
+
+// // console.log(pricesAndTaxes);
+// // #endregion
+
+
+// let priceAndTax = pricesArray.map((price)=>{
+//     return `Price =${price} and tax is ${20/100*price}`
+    
+// })
+
+// console.log(priceAndTax);

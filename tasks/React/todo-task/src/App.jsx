5c4cfa04b2be7b494a0fbeff4ba057411844c026
shamilvasiyev <vasiyevsamil@gmail.com>
2024-08-12 19:54:04 +0400

    Some changes have made

--- tasks/React/todo-task/src/App.jsx
+++ tasks/React/todo-task/src/App.jsx
@@ -1,13 +1,21 @@
-import './App.css';
-import {TodoList} from "./components/todo-list"
-import Header from './components/header';
-import Form from './components/form';
+import "./App.css";
+import { TodoList } from "./components/todo-list";
+import Header from "./components/header";
+import Form from "./components/form";
 function App() {
   return (
     <>
-<div className=" h-lvh"><Header/>
-<main><div className="flex justify-center"><Form /></div>
-<div className=" flex justify-center">  <TodoList /></div></main>
+      <div className=" h-lvh">
+        <Header />
+        <main>
+          <div className="flex justify-center">
+            <Form />
+          </div>
+          <div className=" flex justify-center">
+            {" "}
+            <TodoList />
+          </div>
+        </main>
       </div>
     </>
   );
--- tasks/React/todo-task/src/components/add-todo.jsx
+++ tasks/React/todo-task/src/components/add-todo.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import SingleTodo from "./list-item";
-
 export const AddTodo = () => {
   return (
-    <form id="todo-form" >
+    <form id="todo-form">
       <div className="flex mt-4 mb-4 pl-2 pt-1 pr-2 pb-1 opacity-90">
-        <input 
+        <input
           type="text"
-          className="w-full px-6 py-2 mr-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500" 
-          id="todo-input" 
-          placeholder="Add new task" 
+          className="w-full px-6 py-2 mr-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500"
+          id="todo-input"
+          placeholder="Add new task"
           required
         />
-        <button 
-          type="submit" 
+        <button
+          type="submit"
           className="add-todo bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-1 px-5 rounded-lg"
         >
           Add
